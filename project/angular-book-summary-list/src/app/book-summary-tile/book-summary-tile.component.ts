5d011fbf8188aa6abeee4435ab5791ced0294902
Hoang Linh <truonghoanglinhk55bkhn@gmail.com>
2019-10-13 23:30:42 +0700

    feat: change to scss

--- project/angular-book-summary-list/src/app/book-summary-tile/book-summary-tile.component.ts
+++ project/angular-book-summary-list/src/app/book-summary-tile/book-summary-tile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: BookSummaryTileComponent.SELECTOR,
   templateUrl: './book-summary-tile.component.html',
-  styleUrls: ['./book-summary-tile.component.css'],
+  styleUrls: ['./book-summary-tile.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
 export class BookSummaryTileComponent implements OnInit {

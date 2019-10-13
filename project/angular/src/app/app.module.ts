5d7f4de69383ef5a4f0008b13c362f2d35a24a16
Hoang Linh <truonghoanglinhk55bkhn@gmail.com>
2019-10-13 15:49:51 +0700

    feat: build book-summary-tile component

--- project/angular/src/app/app.module.ts
+++ project/angular/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { createCustomElement } from '@angular/elements';
 import { BookSummaryTileComponent } from './book-summary-tile/book-summary-tile.component';
 
 import 'bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [BookSummaryTileComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CommonModule],
   providers: [],
   entryComponents: [BookSummaryTileComponent] // This we have to do for all dynamic components
 })

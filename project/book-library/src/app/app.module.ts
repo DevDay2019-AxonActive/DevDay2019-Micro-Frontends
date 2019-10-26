48694bd1e85a9c42d3b6fca466f6f6a31fccc19b
Phong Bui <buithanhphongict@gmail.com>
2019-10-26 23:30:33 +0700

    feat: model and integration service

--- project/book-library/src/app/app.module.ts
+++ project/book-library/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA]
---
+++ project/book-library/src/app/integration-services/login.integration.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, ErrorHandler } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { User } from '../models/book.model';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginIntegrationService {
+  private readonly apiUrl = 'http://85.214.44.228:8082/user/';
+
+  constructor(private http$: HttpClient, private errorHandler: ErrorHandler) {}
+
+  public login(username: string, password: string): Observable<User> {
+    const url = this.apiUrl + '/login';
+    const httpParams = new HttpParams();
+    httpParams.set('username', username);
+    httpParams.set('password', password);
+    return this.http$.get<User>(url, { params: httpParams }).pipe(
+      catchError(error => {
+        this.errorHandler.handleError(error);
+        return of(undefined);
+      })
+    );
+  }
+
+  public registration(user: User): Observable<HttpResponse<string>> {
+    const url = this.apiUrl + '/registration';
+    return this.http$.post<HttpResponse<string>>(url, user).pipe(
+      catchError(error => {
+        this.errorHandler.handleError(error);
+        return of(undefined);
+      })
+    );
+  }
+}
